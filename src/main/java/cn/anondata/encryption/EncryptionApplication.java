a25e23ba10a7320cffca6465acf545114d0de6e8
Junjie Zhou <1192031540@qq.com>
2021-11-30 14:33:26 +0800

    initialize repo

---
+++ src/main/java/cn/anondata/encryption/EncryptionApplication.java
@@ -0,0 +1,13 @@
+package cn.anondata.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionApplication.class, args);
+	}
+
+}
