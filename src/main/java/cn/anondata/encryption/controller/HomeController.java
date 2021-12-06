baf7c70eee44c3ed0164b4a03b7c9386997bd9db
Junjie Zhou <1192031540@qq.com>
2021-12-07 00:08:05 +0800

    add home statement

---
+++ src/main/java/cn/anondata/encryption/controller/HomeController.java
@@ -0,0 +1,25 @@
+package cn.anondata.encryption.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HomeController {
+    @GetMapping("/")
+    @PostMapping("/")
+    public String home() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(
+                "<p>This website provides basic cryptography APIs using bouncycastle(https://www.bouncycastle.org/) framework.</p>")
+                .append("\n")
+                .append("<p>Only study purpose is allowed.</p>")
+                .append("\n")
+                .append("<p>Feel free to friendly use and \"No data is collected by this website.\"</p>")
+                .append("\n")
+                .append("<p>More details could be found in repo JunjieChou/encryption(https://github.com/JunjieChou/encryption), and welcome for suggestions and prs.</p>");
+        return sb.toString();
+    }
+}
