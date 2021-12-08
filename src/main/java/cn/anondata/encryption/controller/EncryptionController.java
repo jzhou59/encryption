299a325ca515c25af98d04f74c106aeb969064dc
Junjie Zhou <1192031540@qq.com>
2021-12-08 18:26:54 +0800

    add get support

--- src/main/java/cn/anondata/encryption/controller/EncryptionController.java
+++ src/main/java/cn/anondata/encryption/controller/EncryptionController.java
@@ -1,9 +1,11 @@
 package cn.anondata.encryption.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.anondata.encryption.domain.req.HMACReq;
@@ -27,8 +29,18 @@ public class EncryptionController {
         return hashService.hash(hashReq.getHashAlg(), hashReq.getMessage());
     }
 
+    @GetMapping("/hash")
+    public CommonResp hash(@RequestParam("hashAlg") String hashAlg, @RequestParam("message") String message){
+        return hashService.hash(hashAlg, message);
+    }
+
     @PostMapping("/hmac")
     public CommonResp hmac(@RequestBody HMACReq hmacReq) {
         return hmacService.hmac(hmacReq.getHmacAlg(), hmacReq.getMessage(), hmacReq.getKey());
+    }
+
+    @GetMapping("/hmac")
+    public CommonResp hmac(@RequestParam("hmacAlg") String hmacAlg, @RequestParam("message") String message, @RequestParam("key") String key){
+        return hmacService.hmac(hmacAlg, message, key);
     }
 }
